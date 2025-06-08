8972c32ceb18e05f595eca55c50c22e016b1e8fd
ysato <56924527+ysato@users.noreply.github.com>
2025-06-09 08:11:54 +0900

    feat: add additional tests to MiscTest for str_replace and preg_replace functionality

--- tests/MiscTest.php
+++ tests/MiscTest.php
@@ -40,7 +40,84 @@ EOF;
 
 EOF;
 
-        $actual = str_replace(["$idea"], [''], $contents);
+        $actual = str_replace($idea, '', $contents);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function 短い文言で検索すると意図しない場所を書き換えてしまう()
+    {
+        $expected = <<<'EOF'
+/.idea/*
+!/.idea/codeStyles
+!/.idea/fileTemplates
+!/.idea/inspectionProfiles
+/node_modules
+/.fleet
+/.idea
+/.nova
+
+EOF;
+
+        $search = "es\n";
+
+        $contents = <<<'EOF'
+/.idea/*
+!/.idea/codeStyles
+!/.idea/fileTemplates
+!/.idea/inspectionProfiles
+/node_modules
+/.fleet
+/.idea
+/.nova
+es
+
+EOF;
+
+        $actual = str_replace($search, '', $contents);
+
+        $this->assertNotEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function 行頭、行末を指定し意図した場所のみを置き換える()
+    {
+        $expected = <<<'EOF'
+/.idea/*
+!/.idea/codeStyles
+!/.idea/fileTemplates
+!/.idea/inspectionProfiles
+/node_modules
+/.fleet
+/.idea
+/.nova
+
+EOF;
+
+        # ^は行頭、\Rは全ての種類の改行文字、$で行末を表す
+        # mはマルチライン修飾子
+        # https://www.php.net/manual/ja/reference.pcre.pattern.modifiers.php
+        $search = '/^es\R$/m';
+
+        $contents = <<<'EOF'
+/.idea/*
+!/.idea/codeStyles
+!/.idea/fileTemplates
+!/.idea/inspectionProfiles
+/node_modules
+/.fleet
+/.idea
+/.nova
+es
+
+EOF;
+
+        $actual = preg_replace($search, '', $contents);
 
         $this->assertEquals($expected, $actual);
     }
